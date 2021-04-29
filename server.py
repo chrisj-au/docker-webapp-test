c687981c4cb5f800e32fb2379adf1587c376675f
Christopher Johnston <12930132+chrisj-au@users.noreply.github.com>
2021-04-29 10:54:09 +1000

    Adjusting test text

--- server.py
+++ server.py
@@ -11,16 +11,12 @@ def get_request():
 
 @app.route('/api/service-test/<key>')
 def abn(key):
-    if not key:
-        abort(400)
-
-    results = json.dumps("{'hi':'ok'}")
-    return Response(response=json.dumps(results), mimetype='application/json')
+    return f'Your key is: {key}'
 
 @app.route('/status')
 @app.route('/api/service-test/status')
 def get_status():
-    return "Service is OK"
+    return "Test Service: OK"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80)
